238fe90d992ce0f2111759e545f310ffe58c2b61
sanikazinjad <snzinjad1111@gmail.com>
2020-07-26 10:58:39 +0530

    register, login, logout, home, home_u php files, user DB added

---
+++ register.php
@@ -0,0 +1,205 @@
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = $confirm_password =$name=$Mno= $address="";
+$username_err = $password_err = $confirm_password_err =$name_err=$Mno_err =$address_err="";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+        //validate name
+        if(empty(trim($_POST["name"]))){
+        $name_err = "Please enter a name.";
+    } else{
+        $name = trim($_POST["name"]);
+    } 
+ 
+    // Validate username
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT user_id FROM user WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+
+    //validate mobile number
+    if(empty(trim($_POST["Mno"]))){
+        $Mno_err = "Please enter a valid Mobile number.";    
+    }elseif(strlen(trim($_POST["Mno"])) == 10){
+        $Mno = trim($_POST["Mno"]);
+    }  else{
+        $Mno_err = "Enter valid Mobile number.";
+    }
+
+    //validate address
+    if(empty(trim($_POST["address"]))){
+        $address_err = "Please enter your address.";     
+    } else{
+        $address = trim($_POST["address"]);
+    }
+
+    // Check input errors before inserting in database
+    if(empty($name_err) && empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($Mno_err) && empty($param_address)) {
+        
+        // Prepare an insert statement
+        $sql = "INSERT INTO user (name, username, password, confirm_password, Mno, address) VALUES (?,?,?,?,?,?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ssssss", $param_name, $param_username, $param_password, $param_confirm_password, $param_Mno, $param_address);
+            
+            // Set parameters
+            $param_name=$name;
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_confirm_password = $confirm_password;
+            $param_Mno = $Mno;
+            $param_address = $address;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Sign Up</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <!---<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">--->
+    <style type="text/css">
+        body{ 
+            font-family: 14px Arial, Helvetica, sans-serif;
+            text-align: center;
+            /*background-image: url("sign_bg.jpg");
+            background-position: center;  background-repeat: no-repeat; 
+            background-size: cover;*/
+            background-image:linear-gradient( to bottom, #ffff99 0%, #66ff99 100%);
+        }
+    </style>
+</head>
+<body>
+    
+<div class="wrapper">
+    <style type="text/css">
+    body{ 
+        display: box;     
+        box-align: center; 
+        border: 10px solid white;
+        width: 500px;
+        padding: 20px;
+        margin-left: auto;
+        margin-right: auto;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        marker-bottom: 5px;
+    }
+    </style>
+
+    	<img src="logo1.jpeg" width="100px" height="100px">
+        <h1>ShopMedz</h1>
+        <p>Create accout to <b>STAY HOME & STAY SAFE</b>.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($name_err)) ? 'has-error' : ''; ?>">
+                <label>Name</label>
+                <input type="text" name="name" class="form-control" value="<?php echo $name; ?>">
+                <span class="help-block"><?php echo $name_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($Mno_err)) ? 'has-error' : ''; ?>">
+                <label>Mobile Number</label>
+                <input type="text" name="Mno" class="form-control" value="<?php echo $Mno; ?>">
+                <span class="help-block"><?php echo $Mno_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($address_err)) ? 'has-error' : ''; ?>">
+                <label>Address</label>
+                <input type="text" name="address" class="form-control" value="<?php echo $address; ?>">
+                <span class="help-block"><?php echo $address_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="SIGN UP">
+                
+            </div>
+            <style>body{ padding-top: 30px;}</style>
+            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        </form>
+</div>    
+</body>
+</html>
