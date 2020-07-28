3a6034b6a99fce5f651f39a73b5da6b77e4ca74d
Your Name <you@example.com>
2020-07-28 22:53:13 +0530

    Changed title

--- product.php
+++ product.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Cart</title>
+    <title>ShopBox | Products</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="icon" type="image/ico" href="icons/cart_logo_cir.png">
+    <link rel="icon" type="image/ico" href="icons/logo_ico.png">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
